51d99f475d07250b1129b45c6ffe2df5bf0e2cc0
Tmjk <thomasandyjk@gmail.com>
2018-01-27 18:54:52 +0800

    minor fix

--- Project1/buttonInterface.h
+++ Project1/buttonInterface.h
@@ -8,9 +8,5 @@ public:
 	virtual void checkClick(sf::Vector2f mousePos) = 0;
 	virtual void setCaption(std::string caption) = 0;
 	virtual std::string getCaption() = 0;
-	virtual ~ButtonInterface() = 0;
+	virtual ~ButtonInterface() { };
 };
-
-ButtonInterface::~ButtonInterface() {
-
-}
