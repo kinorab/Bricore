caad7c443f79b45cb9bb0285bc706a3af374e59c
Tmjk <thomasandyjk@gmail.com>
2018-03-13 13:58:12 +0800

    minor change

--- Project1/audio.h
+++ Project1/audio.h
@@ -7,8 +7,4 @@ public:
 	static sf::Sound sound1;
 	static sf::Music bgmusic;
 	static void initialize();
-private:
-	static sf::SoundBuffer buffer1;
-	static float bufferBgVolume;
-	static float bufferVolume1;
 };
