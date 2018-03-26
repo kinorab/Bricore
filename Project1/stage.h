ade9185fde08ae8a650234b5900fcadb334d7baf
Tmjk <thomasandyjk@gmail.com>
2018-03-26 22:52:33 +0800

    minor changes

--- Project1/stage.h
+++ Project1/stage.h
@@ -14,15 +14,15 @@
 class Stage :
 	public Container {
 public:
-	static float blockLength;
-	static float incre1;
 	explicit Stage(sf::RenderWindow & window);
 	virtual ~Stage();
 	virtual void update(float updateSpan, sf::Vector2i mousePosition);
 private:
 	std::shared_ptr<item::Ball> ball;
+	float blockLength;
 	std::shared_ptr<item::Brick> bricks;
 	std::shared_ptr<HUD> hud;
+	float incre1;
 	std::shared_ptr<ParticleSystem> mouseLight;
 	std::shared_ptr<Obstacle> obstacles;
 	std::shared_ptr<Player> player;
