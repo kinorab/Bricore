95efe1441a1be6bf2a8f6dd242c3d1562759ad6c
Tmjk <thomasandyjk@gmail.com>
2018-05-24 00:06:17 +0800

    bug fix

--- BricksEx/spriteNode.cpp
+++ BricksEx/spriteNode.cpp
@@ -9,8 +9,8 @@ namespace game {
 
 	}
 
-	bool SpriteNode::containsPoint(const sf::Vector2f &) const {
-		return false;
+	bool SpriteNode::containsPoint(const sf::Vector2f & point) const {
+		return sprite->getGlobalBounds().contains(getTransform().getInverse().transformPoint(point));
 	}
 
 	std::shared_ptr<sf::Drawable> SpriteNode::getDrawable() const {
