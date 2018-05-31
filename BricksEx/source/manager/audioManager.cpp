53926d82a825fd8b758f746f18d80f36c6091306
kinorab <kinoko5279@gmail.com>
2018-06-01 03:38:34 +0800

    Mini

--- BricksEx/source/manager/audioManager.cpp
+++ BricksEx/source/manager/audioManager.cpp
@@ -18,7 +18,7 @@ AudioManager::~AudioManager() {
 
 void AudioManager::initialize() {
 	// if memory violation happen, reset the lib connector of project (-d have something bug)
-	const std::vector<std::string> fileNames = { "resource/audio/sound/s1.wav", "resource/audio/bgm/bg.wav" };
+	const std::vector<std::string> fileNames = { "res/audio/sound/s1.wav", "res/audio/bgm/bg.wav" };
 	if (!buffer1.loadFromFile(fileNames[0])) {
 		std::cout << "Cannot load " << fileNames[0] <<  std::endl;
 	}
